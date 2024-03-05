0a84e52b85fa3c5cb7399f0fec0ae9da0762482a
MilanKutnogorskiy <kutnogorskiy77@gmail.com>
2024-03-05 18:39:08 +0300

    step 04 change healhttracker

---
+++ healthtrackers/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+
+from healthtrackers.models import Habit
+from healthtrackers.validators import RewardAndHabitValidator, ActionTimeValidator, IsPleasantValidator, PleasantHabitValidator, \
+    PeriodValidator
+
+
+class HabitSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Habit
+        fields = '__all__'
+
+        validators = [
+            RewardAndHabitValidator(reward='reward', habit='associated_habit'),
+            ActionTimeValidator(time='action_time'),
+            IsPleasantValidator(habit='associated_habit'),
+            PleasantHabitValidator(habit_is_nice='is_pleasant_habit', reward='reward', habit='associated_habit'),
+            PeriodValidator(period='periodicity')
+        ]
